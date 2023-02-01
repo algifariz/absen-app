96cd3aa07ed78bbbcfb72023d0faa4c2e963dcc5
algifariz <algifari.zidan19@gmail.com>
2023-02-01 11:49:41 +0700

    menambah delet di jam mengajar

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,7 @@ Route::get('/dashboard', function () {
     return view('pages.dashboard', ['type_menu' => 'dashboard']);
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('/data-guru', function () {
-    $dataguru = ModelsGuru::all();
-    return view('pages.data-guru', [
-        'type_menu' => 'data guru',
-        'title' => 'Data Guru',
-        'guru' => $dataguru
-    ]);
-})->middleware('auth');
+Route::get('/data-guru', [App\Http\Controllers\GuruController::class, 'index'])->middleware('auth');
 
 Route::get('/rekap-absen', function () {
     return view('pages.rekap-absen', [
@@ -43,56 +36,51 @@ Route::get('/rekap-absen', function () {
 
 Route::get('/status-guru', [App\Http\Controllers\PresensiController::class, 'index'])->middleware('auth');
 
-Route::get('/tingkat-honor', function () {
-    $datatingkatan = ModelsTingkatan::all();
-    return view('pages.tingkat-honor', [
-        'type_menu' => 'tingkat honor',
-        'title' => 'Tingkat Honor',
-        'tingkatan' => $datatingkatan
-    ]);
-});
-// Route::get('/data-tunjangan', function () {
-//     return view('pages.data-tunjangan', [
-//         'type_menu' => 'components',
-//         'title' => 'Data Tunjangan',
-//     ]);
-// });
+Route::get('/data-tingkatan', [App\Http\Controllers\TingkatanController::class, 'index'])->middleware(['auth']);
+
 Route::get('/scan', function () {
     return view('pages.scan', [
         'type_menu' => 'scan qr',
         'title' => 'Scan Qr Code',
         'name' => 'Scan Qr Code',
     ]);
-});
+})->middleware(['auth']);
 
 // Guru
-Route::get('/tambah-data', [App\Http\Controllers\GuruController::class, 'tambah']);
-Route::post('/guru/simpan_guru', [App\Http\Controllers\GuruController::class, 'simpan_guru']);
-Route::get('/edit-data/{id}', [App\Http\Controllers\GuruController::class, 'edit']);
-Route::put('/updateGuru/{id}', [App\Http\Controllers\GuruController::class, 'update'])->name('updateGuru');
-Route::delete('/hapusGuru/{id}', [App\Http\Controllers\GuruController::class, 'destroy']);
+Route::get('/tambah-data', [App\Http\Controllers\GuruController::class, 'tambah'])->middleware(['auth']);
+Route::post('/guru/simpan_guru', [App\Http\Controllers\GuruController::class, 'simpan_guru'])->middleware(['auth']);
+Route::get('/edit-data/{id}', [App\Http\Controllers\GuruController::class, 'edit'])->middleware(['auth']);
+Route::put('/updateGuru/{id}', [App\Http\Controllers\GuruController::class, 'update'])->name('updateGuru')->middleware(['auth']);
+Route::delete('/hapusGuru/{id}', [App\Http\Controllers\GuruController::class, 'destroy'])->middleware(['auth']);
 
 // Tingkatan
-Route::get('/tambah-tingkatan', [App\Http\Controllers\TingkatanController::class, 'tambah']);
-Route::post('/tingkatan/simpan_tingkatan', [App\Http\Controllers\TingkatanController::class, 'simpan_tingkatan']);
-Route::get('/edit-tingkatan/{id}', [App\Http\Controllers\TingkatanController::class, 'edit']);
-Route::put('/updateTingkatan/{id}', [App\Http\Controllers\TingkatanController::class, 'update'])->name('updateTingkatan');
-Route::delete('/hapusTingkatan/{id}', [App\Http\Controllers\TingkatanController::class, 'destroy']);
+Route::get('/tambah-tingkatan', [App\Http\Controllers\TingkatanController::class, 'tambah'])->middleware(['auth']);
+Route::post('/tingkatan/simpan_tingkatan', [App\Http\Controllers\TingkatanController::class, 'simpan_tingkatan'])->middleware(['auth']);
+Route::get('/edit-tingkatan/{id}', [App\Http\Controllers\TingkatanController::class, 'edit'])->middleware(['auth']);
+Route::put('/updateTingkatan/{id}', [App\Http\Controllers\TingkatanController::class, 'update'])->name('updateTingkatan')->middleware(['auth']);
+Route::delete('/hapusTingkatan/{id}', [App\Http\Controllers\TingkatanController::class, 'destroy'])->middleware(['auth']);
 
 // Tunjangan
-Route::get('/data-tunjangan', [App\Http\Controllers\TunjanganController::class, 'index']);
-Route::get('/tambah-data-tunjangan', [App\Http\Controllers\TunjanganController::class, 'tambah']);
-Route::post('/tunjangan/simpan_tunjangan', [App\Http\Controllers\TunjanganController::class, 'simpan_tunjangan']);
-Route::get('/edit-data-tunjangan/{id}', [App\Http\Controllers\TunjanganController::class, 'edit']);
-Route::put('/updateTunjangan/{id}', [App\Http\Controllers\TunjanganController::class, 'update'])->name('updateTunjangan');
-Route::delete('/hapusTunjangan/{id}', [App\Http\Controllers\TunjanganController::class, 'destroy']);
+Route::get('/data-tunjangan', [App\Http\Controllers\TunjanganController::class, 'index'])->middleware(['auth']);
+Route::get('/tambah-data-tunjangan', [App\Http\Controllers\TunjanganController::class, 'tambah'])->middleware(['auth']);
+Route::post('/tunjangan/simpan_tunjangan', [App\Http\Controllers\TunjanganController::class, 'simpan_tunjangan'])->middleware(['auth']);
+Route::get('/edit-data-tunjangan/{id}', [App\Http\Controllers\TunjanganController::class, 'edit'])->middleware(['auth']);
+Route::put('/updateTunjangan/{id}', [App\Http\Controllers\TunjanganController::class, 'update'])->name('updateTunjangan')->middleware(['auth']);
+Route::delete('/hapusTunjangan/{id}', [App\Http\Controllers\TunjanganController::class, 'destroy'])->middleware(['auth']);
 
-// Jabatan
-Route::get('/data-jabatan', [App\Http\Controllers\JabatanController::class, 'index']);
+//jam mengajar
+Route::get('/data-jam-mengajar', [App\Http\Controllers\JamMengajarController::class, 'index'])->middleware(['auth']);
+Route::get('/tambah-data-jam-mengajar', [App\Http\Controllers\JamMengajarController::class, 'tambah'])->middleware(['auth']);
+Route::post('/simpan_jam_mengajar', [App\Http\Controllers\JamMengajarController::class, 'simpan_jam_mengajar'])->middleware(['auth']);
+Route::get('/edit-data-jam-mengajar/{id}', [App\Http\Controllers\JamMengajarController::class, 'edit'])->middleware(['auth']);
+Route::put('/update-jam-mengajar/{id}', [App\Http\Controllers\JamMengajarController::class, 'update'])->name('updateJamMengajar')->middleware(['auth']);
+Route::delete('/hapusJamMengajar/{id}', [App\Http\Controllers\JamMengajarController::class, 'destroy'])->middleware(['auth']);
+
+
 
 // Generate QR Code
-Route::get('/generate-qr-code', [App\Http\Controllers\QrController::class, 'index'])->name('generate-qr-code');
-Route::post('/generate-qr-code-guru', [App\Http\Controllers\QrController::class, 'generate_qr_code_guru']);
+Route::get('/generate-qr-code', [App\Http\Controllers\QrController::class, 'index'])->name('generate-qr-code')->middleware(['auth']);
+Route::post('/generate-qr-code-guru', [App\Http\Controllers\QrController::class, 'generate_qr_code_guru'])->middleware(['auth']);
 
 // Register
 Route::get('/register', [App\Http\Controllers\RegisterController::class, 'index']);
@@ -106,8 +94,8 @@ Route::post('/login', [App\Http\Controllers\LoginController::class, 'authenticat
 Route::post('/logout', [App\Http\Controllers\LoginController::class, 'logout']);
 
 //scan
-Route::get('/scan', [App\Http\Controllers\ScanController::class, 'index']);
-Route::post('/validasi', [App\Http\Controllers\ScanController::class, 'validasi'])->name('validasi');
+Route::get('/scan', [App\Http\Controllers\ScanController::class, 'index'])->middleware(['auth']);
+Route::post('/validasi', [App\Http\Controllers\ScanController::class, 'validasi'])->name('validasi')->middleware(['auth']);
 
 //laporan gajih
 Route::get('/laporan-gajih', function () {
