bb0c6c3a3f1c86d1747cbb1f46425fed9b22e509
algifariz <algifari.zidan19@gmail.com>
2023-02-07 15:21:31 +0700

    mengubah sidbar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -48,36 +48,11 @@
       <li class="nav-item dropdown {{ $type_menu === 'data-absen' || $type_menu === 'data-gajih' ? 'active' : '' }}">
         <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
             class="fa-solid fa-money-check-dollar"></i>
-          <span>Trasaksi</span></a>
-        <ul class="dropdown-menu block">
-          <li class="nav-item dropdown {{ $type_menu === 'data-absen' ? 'active' : '' }}">
-            <a class="nav-link" href="{{ url('/tarsaaksi') }}"><i class="fa fa-book"></i></i><span>Data
-                Absensi</span></a>
-          </li>
-          <li class="nav-item dropdown {{ $type_menu === 'data-gajih' ? 'active' : '' }}">
-            <a class="nav-link" href="{{ url('tingkat-honor') }}"><i class="fa fa-dollar"
-                aria-hidden="true"></i><span>Data Gajih</span></a>
-          </li>
-        </ul>
-      </li>
-
-      <li class="menu-header">Laporan</li>
-      <li
-        class="nav-item dropdown {{ $type_menu === 'laporan-absen' || $type_menu === 'laporan-gajih' || $type_menu === 'slip-gajih' ? 'active' : '' }}">
-        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fa-solid fa-newspaper"></i>
           <span>Laporan</span></a>
         <ul class="dropdown-menu block">
-          <li class="nav-item dropdown {{ $type_menu === 'laporan-absen' ? 'active' : '' }}">
-            <a class="nav-link" href=""><i class="fa-solid fa-users"></i></i><span>Laporan
-                Absensi</span></a>
-          </li>
-          <li class="nav-item dropdown {{ $type_menu === 'laporan-gajih' ? 'active' : '' }}">
-            <a class="nav-link" href="laporan-gajih"><i class="fa fa-list-alt" aria-hidden="true"></i><span>Laporan
-                Gajih</span></a>
-          </li>
-          <li class="nav-item dropdown {{ $type_menu === 'slip-gajih' ? 'active' : '' }}">
-            <a class="nav-link" href="/slip-gajih"><i class="fa-solid fa-users"></i></i><span>Slip
-                Gajih</span></a>
+          <li class="nav-item dropdown {{ $type_menu === 'data-gajih' ? 'active' : '' }}">
+            <a class="nav-link" href="{{ url('tingkat-honor') }}"><i class="fa fa-dollar"
+                aria-hidden="true"></i><span>Gaji Guru</span></a>
           </li>
         </ul>
       </li>
